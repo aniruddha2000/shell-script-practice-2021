2e3dc43290baf8adf605661eb3a7d3adf652c7a8
Aniruddha Basak <codewithaniruddha@gmail.com>
2021-05-10 10:24:10 +0530

    Update common_element.js

--- common_element.sh
+++ common_element.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
-
+# enter your array comparison code here
+# initialize arrays a b c
 a=(3 5 8 10 6)
 b=(6 5 4 12)
 c=(14 7 5 7)
-
+#comparison of first two arrays a and b
 for x in "${a[@]}"; do
-    in=false
-    for j in "${b[@]}"; do
-        if [ "$x" == "$y" ]; then
+    # in=false
+    for y in "${b[@]}"; do
+        if [ $x = $y ]; then
+            # assigning the matching results to new array z
             z[${#z[@]}]=$x
         fi
     done
 done
-
+#comparison of third array c with new array z
 for i in "${c[@]}"; do
-    in=false
+    # in=false
     for k in "${z[@]}"; do
-        if [ "$i" == "$k" ]; then
+        if [ $i = $k ]; then
+            # assigning the matching results to new array j
             j[${#j[@]}]=$i
         fi
     done
 done
-
-echo "${j[@]}"
+# print content of array j
+echo ${j[@]}
