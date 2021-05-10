0b7fc2bcdc8e535822e183d54a66f2f4859bcc9f
Aniruddha Basak <codewithaniruddha@gmail.com>
2021-05-11 00:05:32 +0530

    Add user_ip.sh

---
+++ user_ip.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if read -t 10 -sp "Enter secret passphrase > " secret_pass; then
+    echo -e "\nSecret passphrase = '$secret_pass'"
+else
+    echo -e "\nInput timed out" >&2
+    exit 1
+fi
