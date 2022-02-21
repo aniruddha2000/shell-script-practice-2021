3d936578c4a3fdf38cb9c2ab4a947ca24f12b5a1
Aniruddha Basak <codewithaniruddha@gmail.com>
2022-02-21 12:13:54 +0530

    add createReSpawnChild code

--- Assignment8/createReSpawnChild.c
+++ Assignment8/createReSpawnChild.c
@@ -1,28 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
 #include <sys/wait.h>
 
-int hg = 0;
+// int main(void)
+// {
+//   pid_t pid = fork();
 
-int main()
+//   if (pid == 0)
+//   {
+//     printf("Process created PID: %d\n", getpid());
+//   }
+//   else
+//   {
+//     // sleep(5);
+//     kill(pid, SIGTERM);
+//     printf("Process killed\n");
+//   }
+
+//   int status;
+//   pid_t result = waitpid(pid, &status, WNOHANG);
+//   if (result == 0)
+//   {
+//     printf("Child is still alive\n");
+//   }
+//   else if (result == -1)
+//   {
+//     printf("There is something wrong\n");
+//   }
+//   else
+//   {
+//     printf("Child process exited\n");
+//   }
+// }
+
+void main()
 {
-  pid_t parent, child;
-  parent = fork();
-  if (parent == 0)
+  pid_t p;
+  printf("Parent Process PID: %d\n", getpid());
+  int i;
+  for (i = 0; i < 10; i++)
   {
-    hg++;
-    while (hg)
+    p = fork();
+    if (p == 0)
     {
-      hg = 0;
-      parent = fork();
-      if (parent == 0)
-      {
-        hg++;
-      }
-      else
-      {
-        wait(NULL);
-      }
+      printf("New child Process running with PID: %d\n", getpid());
+      sleep(2);
+      printf("Current Child Process ends.\n");
+
+      return;
+    }
+    else
+    {
+      wait(NULL);
     }
   }
-  return 0;
 }
